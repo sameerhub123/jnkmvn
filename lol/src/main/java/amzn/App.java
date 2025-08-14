04a6dffbb659ba058de2db422eab70ada32cc78a
sameerhub123 <sameermokashi06@gmail.com>
2025-08-14 18:28:19 +0530

    Update App.java

--- lol/src/main/java/amzn/App.java
+++ lol/src/main/java/amzn/App.java
@@ -1,10 +1,29 @@
 package amzn;
 
-/**
- * Hello world!
- */
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) {
+                try {
+                    String response = "Hello from EC2!";
+                    exchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started at http://localhost:8080/");
     }
 }
